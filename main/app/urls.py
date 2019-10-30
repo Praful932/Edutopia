ddfcd0b1097a9680749e2ec6c2cdaabc99025fe7
Praful Mohanan <praful.mohanan@gmail.com>
2019-10-30 12:08:35 +0530

    Virtual env

--- main/app/urls.py
+++ main/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# django.urls expects an app as an argument
+
 urlpatterns=[
     path("",views.index)
 ]
--- main/main/settings.py
+++ main/main/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [  
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
