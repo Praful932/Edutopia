daff2ab694bedd0352ee27146dc55108fbef6f74
Praful Mohanan <praful.mohanan@gmail.com>
2019-11-01 17:07:15 +0530

    scale changes

--- Edutopia/urls.py
+++ Edutopia/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("",include("app.urls")),
+    path("",include("app.urls"),name="home"),
     path('admin/', admin.site.urls)
 ]
