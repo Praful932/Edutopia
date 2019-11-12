da67ed9d1a3d773c0bb577823ccd56f22dfbedc2
Praful Mohanan <praful.mohanan@gmail.com>
2019-11-12 21:29:37 +0530

    Added models

--- app/models.py
+++ app/models.py
@@ -3,9 +3,24 @@ from django.contrib.auth.models import  AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    is_student = models.BooleanField(default=False)
-    is_mentor  = models.BooleanField(default=False)
+    is_student=models.BooleanField(default=False)
+    is_mentor=models.BooleanField(default=False)
+    role=models.CharField('Current Role',max_length=255)
+    domains=models.CharField('Domain',max_length=255)
 
 class Student(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
-    interests = models.ManyToManyField()
+    user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+
+class Mentor(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+
+class Post(models.Model):
+    owner=models.ForeignKey(User,on_delete=models.CASCADE)
+    date=models.DateTimeField(auto_now_add=True)
+    # domains=models.
+    
+
+
+    
+
+
