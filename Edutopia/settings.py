0c26b25532cdd3c96dff9328332e732da8adf8ea
Praful Mohanan <praful.mohanan@gmail.com>
2019-11-15 22:35:02 +0530

    Configured and added models

--- Edutopia/settings.py
+++ Edutopia/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+AUTH_USER_MODEL="app.User"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -102,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL='users.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
--- app/models.py
+++ app/models.py
@@ -1,24 +1,41 @@
 from django.db import models
-from django.contrib.auth.models import  AbstractUser
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+# on_delete=models.CASCADE when a foreign key's table is deleted delete all records if
+# any associated with it
+
+# using related name(defined in class a) it is possible to access all the objects related in class a from class b
+
+# using blank=True makes to user associated with no domains
+
 class User(AbstractUser):
-    is_student=models.BooleanField(default=False)
-    is_mentor=models.BooleanField(default=False)
-    role=models.CharField('Current Role',max_length=255)
-    domains=models.CharField('Domain',max_length=255)
+    is_student=models.BooleanField('student status',default=False)
+    is_mentor=models.BooleanField('mentor status',default=False)
+
+class Domain(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=100)
+    domaintrack=models.URLField(max_length=200)
 
 class Student(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    domain=models.OneToOneField(Domain,on_delete=models.CASCADE)
+    prof=models.TextField(max_length=50)
+    
 
 class Mentor(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    domains=models.ManyToManyField(Domain,blank=False,related_name="experts")
+
 
 class Post(models.Model):
-    owner=models.ForeignKey(User,on_delete=models.CASCADE)
-    date=models.DateTimeField(auto_now_add=True)
-    # domains=models.
-    
+    topic=models.CharField(max_length=100)
+    content=models.TextField(max_length=1000)
+    created_at=models.DateTimeField(auto_now_add=True)
+    last_updated=models.DateTimeField(auto_now=True)
+    domain=models.ForeignKey(Domain,on_delete=models.CASCADE,related_name="topicposts")
+    owner=models.ForeignKey(Mentor,on_delete=models.CASCADE,related_name="postsby")
 
 
     
