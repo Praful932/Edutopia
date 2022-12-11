ff4d29fc3c66ddea5d50782d093d8fac71ff2671
Praful932 <praful.mohanan@gmail.com>
2022-12-11 12:46:45 +0530

    change static dir

--- Edutopia/settings.py
+++ Edutopia/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # use bs4
 CRISPY_TEMPLATE_PACK = "bootstrap4"
